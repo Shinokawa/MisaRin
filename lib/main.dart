9fd8163ee93b00a7d7805d20cd6299bd78bf6b6f
MCDFSteve <2911898435@qq.com>
2025-11-01 21:32:38 +0800

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,30 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'app/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    await windowManager.ensureInitialized();
+
+    const windowOptions = WindowOptions(
+      titleBarStyle: TitleBarStyle.hidden,
+      backgroundColor: Color(0x00000000),
+      skipTaskbar: false,
+    );
+
+    await windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.setAsFrameless();
+      await windowManager.maximize();
+      await windowManager.show();
+      await windowManager.focus();
+    });
+  }
+
   runApp(const MisarinApp());
 }
